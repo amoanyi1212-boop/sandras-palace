171ae6846826ec86fc5b50df3c7cba1fb2baeb0f
Nana Yaw <amoanyi1212@gmail.com>
2026-08-10 12:08:25 +0000

    Added PWA - Install as App support

---
+++ static/sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "esirifuahs-palace-v1"; 
+const urlsToCache = ["/", "/static/manifest.json", "/static/icon-192.png", "/static/icon-512.png"]; 
+ 
+self.addEventListener("install", function(event) { 
+    event.waitUntil( 
+        caches.open(CACHE_NAME).then(function(cache) { 
+            return cache.addAll(urlsToCache); 
+        }) 
+    ); 
+}); 
+ 
+self.addEventListener("fetch", function(event) { 
+    event.respondWith( 
+        caches.match(event.request).then(function(response) { 
+            if (response) { return response; } 
+            return fetch(event.request); 
+        }) 
+    ); 
+}); 
+ 
+self.addEventListener("activate", function(event) { 
+    event.waitUntil( 
+        caches.keys().then(function(cacheNames) { 
+            return Promise.all( 
+                cacheNames.filter(function(cacheName) { 
+                    return cacheName !== CACHE_NAME; 
+                }).map(function(cacheName) { 
+                    return caches.delete(cacheName); 
+                }) 
+            ); 
+        }) 
+    ); 
+}); 
